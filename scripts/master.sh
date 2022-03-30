b6069ecfa986c280417ede51e8bae9e33f84091c
mayank4t <mayank.geit89@gmail.com>
2022-03-30 23:07:33 +0530

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -28,6 +28,5 @@ kubeadm init --apiserver-advertise-address=$masterip --pod-network-cidr=192.168.
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-echo "Script execution completed"
+export KUBECONFIG=/etc/kubernetes/admin.conf
 echo "------------Master Node configured ------- "
