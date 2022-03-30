e62859c2910ca1c58227ec1a69eda0099e4f6368
mayank4t <mayank.geit89@gmail.com>
2022-03-30 15:59:09 +0530

    Update worker.sh

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -24,7 +24,3 @@ https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 yum install -y kubeadm-1.18.5-0 kubelet-1.18.5-0 kubectl-1.18.5-0
 systemctl enable --now kubelet
-gcloud compute scp --recurse master:/tmp/mastertoken.sh mastertoken.sh --project=kubernetestestmayank --zone=asia-south2-a
-chmod +x /tmp/mastertoken.sh
-gcloud compute scp --recurse ./mastertoken.sh node04:/tmp/mastertoken.sh --project=kubernetestestmayank --zone=asia-south2-a
-/tmp/mastertoken.sh
