da8311af78f8cdfb1df7cabe4638001088cf1220
mayank4t <mayank.geit89@gmail.com>
2022-03-31 01:42:08 +0530

    Update gcpsetup.sh

--- scripts/gcpsetup.sh
+++ scripts/gcpsetup.sh
@@ -7,10 +7,10 @@ gcloud compute ssh --zone "asia-south2-a" "master"  --project "kubernetestestmay
 if [ $? -ne 0 ];
 then
         gcloud compute instances create master --project=kubernetestestmayank --image=centos-7 --machine-type=e2-medium --zone=asia-south2-a --metadata=startup-script-url=https://raw.githubusercontent.com/mayank4t/kubernetes/master/scripts/master.sh
-        gcloud compute --project=kubernetestestmayank instances get-serial-port-output master --zone=asia-south2-a --port=1 | grep "------------Master Node configured -------"
+        gcloud compute --project=kubernetestestmayank instances get-serial-port-output master --zone=asia-south2-a --port=1 | grep -e "------------Master Node configured -------"
         while [ $? -ne 0 ];
         do
-                gcloud compute --project=kubernetestestmayank instances get-serial-port-output master --zone=asia-south2-a --port=1 | grep "------------Master Node configured -------"
+                gcloud compute --project=kubernetestestmayank instances get-serial-port-output master --zone=asia-south2-a --port=1 | grep -e  "------------Master Node configured -------"
                 echo "working on master...";
                 sleep 2 ;
         done
@@ -28,10 +28,10 @@ do
         instance=node$i
         echo "creation of $instance begain"
         gcloud compute instances create $instance --project=kubernetestestmayank --image=centos-7 --machine-type=e2-micro --zone=asia-south2-a --metadata=startup-script-url=https://raw.githubusercontent.com/mayank4t/kubernetes/master/scripts/worker.sh
-        gcloud compute --project=kubernetestestmayank instances get-serial-port-output $instance --zone=asia-south2-a --port=1 | grep "------------Worker Node configured -------"
+        gcloud compute --project=kubernetestestmayank instances get-serial-port-output $instance --zone=asia-south2-a --port=1 | grep -e "------------Worker Node configured -------"
         while [ $? -ne 0 ];
         do
-                 gcloud compute --project=kubernetestestmayank instances get-serial-port-output $instance --zone=asia-south2-a --port=1 | grep "------------Worker Node configured -------"
+                 gcloud compute --project=kubernetestestmayank instances get-serial-port-output $instance --zone=asia-south2-a --port=1 | grep -e "------------Worker Node configured -------"
                 echo "working on worker...";
                 sleep 2 ;
         done
@@ -39,6 +39,6 @@ do
         gcloud compute scp --recurse ./mastertoken.sh $instance:/tmp/mastertoken.sh --project=kubernetestestmayank --zone=asia-south2-a ;
         gcloud compute ssh --zone "asia-south2-a" "$instance"  --project "sudo kubernetestestmayank" --command "/tmp/mastertoken.sh" ;
         echo "worker node created"
-        $i=i+1
+        i=$i+1
 done
 echo "All Worker node setup complete check using kubectl on master" ;
