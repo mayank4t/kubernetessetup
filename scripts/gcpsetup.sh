3db0d5369244d61978948e16ffcc32f9de33a274
mayank4t <mayank.geit89@gmail.com>
2022-04-07 08:23:39 +0530

    Update gcpsetup.sh

--- scripts/gcpsetup.sh
+++ scripts/gcpsetup.sh
@@ -3,17 +3,24 @@ echo "This script will setup a master node as well"
 echo "If already configured it will skip master node configuration"
 echo "How many worker node you want to setup enter number:-"
 read numberofvm ;
+
+# Check for master node with hostname :- master is configured or not 
 gcloud compute ssh --zone "asia-south2-a" "master"  --project "kubernetestestmayank" --command "sudo kubeadm token create --print-join-command" > /dev/null;
+
+# if master node is not configured it will start creating it using hostname as master
 if [ $? -ne 0 ];
 then
         gcloud compute instances create master --project=kubernetestestmayank --image=centos-7 --machine-type=e2-medium --zone=asia-south2-a --metadata=startup-script-url=https://raw.githubusercontent.com/mayank4t/kubernetessetup/master/scripts/master.sh
-        gcloud compute --project=kubernetestestmayank instances get-serial-port-output master --zone=asia-south2-a --port=1 | grep -e "------------Master Node configured -------"
+        # Waiting for Completion of Master node configuration
+	gcloud compute --project=kubernetestestmayank instances get-serial-port-output master --zone=asia-south2-a --port=1 | grep -e "------------Master Node configured -------"
         while [ $? -ne 0 ];
         do
                 echo "working on master...";
-                sleep 2 ;
+                sleep 10 ;
                 gcloud compute --project=kubernetestestmayank instances get-serial-port-output master --zone=asia-south2-a --port=1 | grep -e  "------------Master Node configured -------"
         done
+	
+	# Check for master node configuration for token  
         gcloud compute ssh --zone "asia-south2-a" "master"  --project "kubernetestestmayank" --command "sudo kubeadm token create --print-join-command" ;
         if [ $? -eq 0 ];
         then
@@ -22,20 +29,26 @@ then
                 echo "There are some issue with the Master node, Please check" ;
         fi
 fi
+# Loop for creation of worker node (hostname is like :- node1, node2 , node3 …)
 for ((i=1 ; i <=$numberofvm ; i++));
 do
         instance=node$i
         echo "creation of $instance begain"
-        gcloud compute instances create $instance --project=kubernetestestmayank --image=centos-7 --machine-type=e2-micro --zone=asia-south2-a --metadata=startup-script-url=https://raw.githubusercontent.com/mayank4t/kubernetessetup/master/scripts/worker.sh
-        gcloud compute --project=kubernetestestmayank instances get-serial-port-output $instance --zone=asia-south2-a --port=1 | grep -e "------------Worker Node configured -------"
+        # creation of worker node
+	gcloud compute instances create $instance --project=kubernetestestmayank --image=centos-7 --machine-type=e2-micro --zone=asia-south2-a --metadata=startup-script-url=https://raw.githubusercontent.com/mayank4t/kubernetessetup/master/scripts/worker.sh
+        # Waiting for Completion of worker node configuration
+	gcloud compute --project=kubernetestestmayank instances get-serial-port-output $instance --zone=asia-south2-a --port=1 | grep -e "------------Worker Node configured -------"
         while [ $? -ne 0 ];
         do
                 echo "working on worker...";
-                sleep 2 ;
+                sleep 10 ;
                 gcloud compute --project=kubernetestestmayank instances get-serial-port-output $instance --zone=asia-south2-a --port=1 | grep -e "------------Worker Node configured -------"
         done
+	# get join command for worker node from master node	
         gcloud compute ssh --zone "asia-south2-a" "master"  --project "kubernetestestmayank" --command "sudo kubeadm token create --print-join-command" >> mastertoken.sh
+	# copy token to Newley created worker node
         gcloud compute scp --recurse ./mastertoken.sh $instance:/tmp/mastertoken.sh --project=kubernetestestmayank --zone=asia-south2-a ;
+	# Join as a node using token
         gcloud compute ssh --zone "asia-south2-a" "$instance"  --project "sudo kubernetestestmayank" --command "/tmp/mastertoken.sh" ;
         rm -rf mastertoken.sh
         echo "worker node created"
